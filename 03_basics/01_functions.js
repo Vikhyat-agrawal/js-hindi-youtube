c15972ad627956590d6213acbf971c444d853272
Vikhyat Agrawal <169044011+Vikhyat-agrawal@users.noreply.github.com>
2025-04-15 20:34:43 +0000

    ... means

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -43,7 +43,8 @@ function calculateCartPrice(val1, val2, ...num1){
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+// console.log(calculateCartPrice(200, 400, 500, 2000))   
+// ... it means the rest of the array 
 
 const user = {
     username: "hitesh",
